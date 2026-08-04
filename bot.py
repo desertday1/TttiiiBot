abf9abe684b3a7f351e3df45b4a1b8a634efd078
desertday1 <desertday1@gmail.com>
2026-08-04 21:40:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
 import os
 import pywikibot
+from pywikibot.login import ClientLoginManager
+
+site = pywikibot.Site("ar", "wikipedia")
 
 username = os.environ["WIKI_USERNAME"]
 password = os.environ["WIKI_PASSWORD"]
 
-site = pywikibot.Site("ar", "wikipedia")
+login_manager = ClientLoginManager(
+    site=site,
+    user=username,
+    password=password
+)
 
-site.login(user=username, password=password)
+login_manager.login()
 
 print("تم تسجيل الدخول بنجاح")
+print("الحساب:", site.username())
 
 page = pywikibot.Page(site, "مستخدم:TttiiiBot/اختبار")
 
@@ -16,4 +24,4 @@ page.text = "هذا تعديل تجريبي من بوت TttiiiBot.\n"
 
 page.save("اختبار اتصال البوت وتعديلاته")
 
-print("تم الحفظ")
+print("تم الحفظ بنجاح")
