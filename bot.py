6441b0adffd5ad474d9af4c4664f35ac2b820d4e
desertday1 <desertday1@gmail.com>
2026-08-04 18:43:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import pywikibot
 
+# اسم الويكي
+site = pywikibot.Site("ar", "wikipedia")
+
+# تسجيل الدخول بالحساب
+site.login()
+
+print("تم الاتصال بويكيبيديا العربية بنجاح")
+
+# اختبار قراءة صفحة
+page = pywikibot.Page(site, "ويكيبيديا")
+
+if page.exists():
+    print("تم العثور على الصفحة:")
+    print(page.title())
+else:
+    print("الصفحة غير موجودة")
