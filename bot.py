68b9b3e6f66fd7039780715fff9665c64e48a3a8
desertday1 <desertday1@gmail.com>
2026-08-04 19:51:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,16 @@
 import os
 import pywikibot
 
-# الاتصال بويكيبيديا العربية
 site = pywikibot.Site("ar", "wikipedia")
 
-# تسجيل الدخول باستخدام البيانات الموجودة في GitHub Secrets
 username = os.environ.get("WIKI_USERNAME")
 password = os.environ.get("WIKI_PASSWORD")
 
-site.login(username=username, password=password)
+site.login()
 
 print("تم تسجيل الدخول بنجاح")
 print("الحساب المستخدم:", site.username())
 
-# اختبار قراءة صفحة
 page = pywikibot.Page(site, "ويكيبيديا")
 
 if page.exists():
