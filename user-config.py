c3e215e15944db78bf77c6db61d19fedc570b5dd
desertday1 <desertday1@gmail.com>
2026-08-05 01:59:13 +0300

    Update user-config.py

--- user-config.py
+++ user-config.py
@@ -1,6 +1,3 @@
-family = "wikipedia"
-mylang = "ar"
-
-usernames['wikipedia']['ar'] = 'TttiiiBot@TttiiiBot'
-
-authenticate['ar.wikipedia.org'] = ('TttiiiBot@TttiiiBot', 'PASSWORD')
+family = 'wikipedia'
+mylang = 'ar'
+usernames['wikipedia']['ar'] = 'TttiiiBot'
