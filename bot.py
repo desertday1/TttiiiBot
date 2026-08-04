5b9027d75ee034c9a569422c23ea3576995a9c49
desertday1 <desertday1@gmail.com>
2026-08-04 20:55:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,16 @@
-import os
 import pywikibot
-from pywikibot.login import ClientLoginManager
 
 site = pywikibot.Site("ar", "wikipedia")
 
-username = os.environ.get("WIKI_USERNAME")
-password = os.environ.get("WIKI_PASSWORD")
-
-login_manager = ClientLoginManager(site=site, user=username)
-login_manager.password = password
-login_manager.login()
+site.login()
 
 print("تم تسجيل الدخول بنجاح")
 print("الحساب المستخدم:", site.username())
 
 page = pywikibot.Page(site, "ويكيبيديا")
 
-print(page.title())
+if page.exists():
+    print("تم العثور على الصفحة:")
+    print(page.title())
+else:
+    print("الصفحة غير موجودة")
