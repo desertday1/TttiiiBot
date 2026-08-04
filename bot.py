843b1e7ea6d2f398e51ae961644cc1c330c4760b
desertday1 <desertday1@gmail.com>
2026-08-04 21:33:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import pywikibot
 
 site = pywikibot.Site("ar", "wikipedia")
 
+site.login()
+
 page = pywikibot.Page(site, "مستخدم:TttiiiBot/اختبار")
 
 text = "هذا تعديل تجريبي من بوت TttiiiBot.\n"
